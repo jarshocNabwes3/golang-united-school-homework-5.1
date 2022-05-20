ba4cf4facd29671ff79f9f20a1789a811e7309a5
Peter Vereshagin <peter@vereshagin.org>
2022-05-20 07:14:31 +0000

    test: :test_tube: result to be equal to expected

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module square
 
 go 1.18
+
+require (
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gotest.tools v2.2.0+incompatible // indirect
+)
--- square_test.go
+++ square_test.go
@@ -2,8 +2,15 @@ package square
 
 import (
 	"testing"
+
+	"gotest.tools/assert"
 )
 
 func TestEnd(t *testing.T) {
-
+	square := Square{Point{1, 2}, 3}
+	end := square.End()
+	assert.Equal(t, end, Point{4, 5},
+		`End differs for 'Point{%v, %v}, %v': '%v' not '%v'`,
+		1, 2, 3, end, Point{4, 5},
+	)
 }
