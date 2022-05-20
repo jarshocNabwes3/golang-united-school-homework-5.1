334ec8d18572a3c441632322595851fcfe45db67
Peter Vereshagin <peter@vereshagin.org>
2022-05-20 07:19:40 +0000

    test: :test_tube: to match given number

--- square_test.go
+++ square_test.go
@@ -14,3 +14,12 @@ func TestEnd(t *testing.T) {
 		1, 2, 3, end, Point{4, 5},
 	)
 }
+
+func TestArea(t *testing.T) {
+	square := Square{Point{1, 2}, 3}
+	area := square.Area()
+	assert.Equal(t, area, 9,
+		`Area() differs for 'Point{%v, %v}, %v': '%v' not '%v'`,
+		1, 2, 3, area, 9,
+	)
+}
